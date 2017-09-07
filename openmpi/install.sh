435f9b2163480cdc2aa66490528de23752f9af75
Matthieu Boileau <boileau@atlas.math.unistra.fr>
2017-09-07 12:05:12 +0200

    Update openmpi

--- openmpi/install.sh
+++ openmpi/install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 set -x 
-export LIB_NAME="openmpi"
-export LIB_VERSION=2.1.1
-export GCC_VERSION=6.4.0
+LIB_NAME="openmpi"
+LIB_VERSION=2.1.1
+GCC_VERSION=6.4.0
 
-export LIB_FULLNAME=${LIB_NAME}-${LIB_VERSION}
-export GCC_VERSION_SHORT="${GCC_VERSION//.}"
-export SUB_DIR=${LIB_NAME}/${LIB_VERSION}/gcc-${GCC_VERSION}
+LIB_FULLNAME=${LIB_NAME}-${LIB_VERSION}
+GCC_VERSION_SHORT="${GCC_VERSION//.}"
+SUB_DIR=${LIB_NAME}/${LIB_VERSION}/gcc-${GCC_VERSION}
 WORK_DIR=/data/software/sources/${SUB_DIR}
 SRC_DIR=${WORK_DIR}/${LIB_FULLNAME}
 ARCHIVE=${SRC_DIR}.tar.gz
 URL="https://www.open-mpi.org/software/ompi/v2.1/downloads/${LIB_NAME}-${LIB_VERSION}.tar.gz"
 BUILD_DIR=${WORK_DIR}/${LIB_FULLNAME}-build
-export INSTALL_DIR=/data/software/install/${SUB_DIR}
+INSTALL_DIR=/data/software/install/${SUB_DIR}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MODULE_DIR=/data/software/modules/mpi/${LIB_NAME}
 MODULE_PATH=${MODULE_DIR}/${LIB_VERSION}_gcc${GCC_VERSION_SHORT}
 
-module load gcc/6.4.0
+module purge
+module load gcc/${GCC_VERSION}
 
 gcc --version
 
@@ -44,4 +45,8 @@ make -j install || exit 1
 
 cd $SCRIPT_DIR
 mkdir -p ${MODULE_DIR}
+
+export LIB_NAME
+export LIB_VERSION
+export INSTALL_DIR
 envtpl  --keep-template -o $MODULE_PATH module.tmpl
