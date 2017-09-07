ee7f64815458d5cc9b7884a1c1b81588e7ce5cd6
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-09-07 12:11:29 +0200

    Add hdf5

---
+++ hdf5/install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -x 
+LIB_NAME="hdf5"
+LIB_VERSION=1.10.1
+GCC_VERSION=6.4.0
+MPI_LIB=openmpi
+MPI_VERSION=2.1.1
+
+LIB_FULLNAME=${LIB_NAME}-${LIB_VERSION}
+GCC_FULL=gcc-$GCC_VERSION
+GCC_SHORT="gcc${GCC_VERSION//.}"
+MPI_FULL=${MPI_LIB}-${MPI_VERSION}
+MPI_SHORT="${MPI_LIB}${MPI_VERSION//.}"
+SUB_DIR=${LIB_NAME}/${LIB_VERSION}/${GCC_FULL}/${MPI_FULL}
+WORK_DIR=/data/software/sources/${SUB_DIR}
+SRC_DIR=${WORK_DIR}/${LIB_FULLNAME}
+ARCHIVE=${SRC_DIR}.tar.gz
+URL="https://www.hdfgroup.org/package/gzip/?wpdmdl=4301"
+BUILD_DIR=${WORK_DIR}/${LIB_FULLNAME}-build
+INSTALL_DIR=/data/software/install/${SUB_DIR}
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MODULE_DIR=/data/software/modules/libs/${LIB_NAME}
+MODULE_PATH=${MODULE_DIR}/${LIB_VERSION}_${GCC_SHORT}_${MPI_SHORT}
+
+module purge
+module load gcc/${GCC_VERSION}
+module load ${MPI_LIB}/${MPI_VERSION}_${GCC_SHORT}
+
+gcc --version |head -n 1
+sleep 2
+mpirun --version |head -n 1
+sleep 2
+
+if [[ ! -f $ARCHIVE ]]; then
+  mkdir -p $WORK_DIR
+  wget $URL -O $ARCHIVE
+fi
+
+if [[ ! -d $SRC_DIR ]]; then
+  tar zxf $ARCHIVE --directory $WORK_DIR
+fi
+
+if [[ ! -d $BUILD_DIR ]]; then
+  mkdir $BUILD_DIR
+  cd $BUILD_DIR
+  CC=`which mpicc` CXX=`which mpic++` FC=`which mpif90` \
+  ${SRC_DIR}/configure \
+  --enable-parallel \
+  --enable-fortran \
+  --enable-fortran2003
+  --enable-build-all \
+  --enable-production \
+  --prefix=${INSTALL_DIR} || exit 1
+fi
+
+cd $BUILD_DIR
+make -j || exit 1
+make -j install || exit 1
+
+cd $SCRIPT_DIR
+mkdir -p ${MODULE_DIR}
+
+export LIB_NAME
+export LIB_VERSION
+export INSTALL_DIR
+envtpl  --keep-template -o $MODULE_PATH module.tmpl
