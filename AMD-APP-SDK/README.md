6e64e25c0eccfdf81caea51b5bde7fa711cfd30e
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-09-11 17:17:11 +0200

    Install OpenCL for AMD CPUs

---
+++ AMD-APP-SDK/README.md
@@ -0,0 +1,2 @@
+- Download installation script on <http://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/> to `/data/software/sources/AMD-APP-SDK`
+- Run `./install.sh` script as a `:software` user. Answer "yes", "yes" and for example `/data/software/install/AMD-APP-SDK/3.0.130.136` to the questions
---
+++ AMD-APP-SDK/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x 
+LIB_NAME="AMD-APP-SDK"
+LIB_VERSION=3.0.130.136
+
+SUB_DIR=${LIB_NAME}/${LIB_VERSION}
+SRC_DIR=/data/software/sources/${LIB_NAME}
+TARBALL="${LIB_NAME}Installer-v${LIB_VERSION}-GA-linux64.tar.bz2"
+INSTALL_SCRIPT="${LIB_NAME}-v${LIB_VERSION}-GA-linux64.sh"
+TARGET_DIR=/data/software/install/${SUB_DIR}
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_DIR=${TARGET_DIR}/AMDAPPSDK-3.0
+MODULE_DIR=/data/software/modules/libs/${LIB_NAME}
+MODULE_PATH=${MODULE_DIR}/${LIB_VERSION}
+
+
+if [[ -d $SRC_DIR ]]; then
+  cd $SRC_DIR
+  if [[ ! -f $INSTALL_SCRIPT ]]; then
+    tar xvjf $TARBALL || exit 1
+  fi
+  ./$INSTALL_SCRIPT --nox11 --target $TARGET_DIR
+  echo $INSTALL_DIR/lib/x86_64/sdk/libamdocl64.so | tee  $INSTALL_DIR/etc/OpenCL/vendors/amd64.icd
+else
+  echo "Source dir $SRC_DIR does not exist."
+  exit 1
+fi
+
+
+cd $SCRIPT_DIR
+mkdir -p $MODULE_DIR
+
+export INSTALL_DIR
+envtpl  --keep-template -o $MODULE_PATH module.tmpl
