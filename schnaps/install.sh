23d0ea4c5b975cbaa9bfc7abd16363c61e12fe76
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-09-22 10:38:56 +0200

    Add schnaps.profile

---
+++ schnaps/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x 
+LIB_NAME="schnaps.profile"
+GCC_VERSION=6.4.0
+MPI_LIB=openmpi
+MPI_VERSION=1.10.7
+
+GCC_FULL=gcc-$GCC_VERSION
+GCC_SHORT="gcc${GCC_VERSION//.}"
+MPI_FULL=${MPI_LIB}-${MPI_VERSION}
+MPI_SHORT="${MPI_LIB}${MPI_VERSION//.}"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MODULE_DIR=/data/software/modules/profiles
+MODULE_PATH=${MODULE_DIR}/${LIB_NAME}
+
+install_module()
+{
+cd $SCRIPT_DIR
+mkdir -p ${MODULE_DIR}
+
+export GCC_VERSION
+export GCC_SHORT
+export MPI_LIB
+export MPI_SHORT
+export MPI_VERSION
+export LIB_NAME
+
+envtpl  --keep-template -o $MODULE_PATH module.tmpl
+}
+
+install_module
+
